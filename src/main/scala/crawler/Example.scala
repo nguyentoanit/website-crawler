5144114c847430f3276d83be68a2b031c98f177f
toan_nv <nguyentoandev@gmail.com>
2018-12-17 09:45:11 +0700

    Create url List and remove duplicate element

--- src/main/scala/crawler/Example.scala
+++ src/main/scala/crawler/Example.scala
@@ -6,17 +6,24 @@ import org.jsoup.nodes.Element
 import org.jsoup.select.Elements
 
 object Hello extends App {
-  // var url =
+  val root: String = "https://www.google.com"
+  var urls: List[String] = List()
   var doc: Document = Jsoup
     .connect(
-      "https://www.google.com/search?q=site:github.com/nguyentoanit&ei=jIATXLOQGJyQvQT79argAw&start=0&sa=N&ved=0ahUKEwizhvmhiZ_fAhUcSI8KHfu6Cjw4FBDy0wMIQQ&biw=1680&bih=869")
+      root + "/search?q=site:github.com/nguyentoanit&ei=jIATXLOQGJyQvQT79argAw&start=0&sa=N&ved=0ahUKEwizhvmhiZ_fAhUcSI8KHfu6Cjw4FBDy0wMIQQ&biw=1680&bih=869")
     .get();
+  urls = urls :+ "/search?q=site:github.com/nguyentoanit&ei=jIATXLOQGJyQvQT79argAw&start=0&sa=N&ved=0ahUKEwizhvmhiZ_fAhUcSI8KHfu6Cjw4FBDy0wMIQQ&biw=1680&bih=869"
   var navs: Elements = doc.select("#nav tbody tr td a");
   navs.forEach(
     nav => {
       // Println Attribute of element
-      println(nav.attr("href"))
-      println
+      urls = urls :+ nav.attr("href")
+
     }
   )
+
+  // Remove duplicate elements
+  println(urls.distinct)
+  println
+
 }
